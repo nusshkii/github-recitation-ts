d22206330e6f104bf9f99a60d563b9e407fbd722
Anushka <anushkasatpute2080@gmail.com>
2023-08-24 19:25:23 +0300

    changed the packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,33 @@
 module.exports = {
-  env: {
-    es2021: true,
-    node: true,
-    jest: true,
+  "env": {
+      "es2021": true,
+      "node": true
   },
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+  "extends": [
+      "eslint:recommended",
+      "plugin:@typescript-eslint/recommended"
   ],
-  ignorePatterns: ["*.js", "*.test.ts"],
-
-  overrides: [],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-    project: ["tsconfig.json"],
+  "overrides": [
+      {
+          "env": {
+              "node": true
+          },
+          "files": [
+              ".eslintrc.{js,cjs}"
+          ],
+          "parserOptions": {
+              "sourceType": "script"
+          }
+      }
+  ],
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+      "ecmaVersion": "latest",
+      "sourceType": "module"
   },
-  plugins: ["@typescript-eslint"],
-  rules: {},
-};
+  "plugins": [
+      "@typescript-eslint"
+  ],
+  "rules": {
+  }
+}
