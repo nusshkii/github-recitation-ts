1122b71196ce008654c838e3941f56cd8ff8eb74
aisha kh <akhaldi@andrew.cmu.edu>
2023-08-24 14:40:37 +0300

    downloaded eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,33 @@
 module.exports = {
-  env: {
-    es2021: true,
-    node: true,
-    jest: true,
-  },
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:@typescript-eslint/recommended-requiring-type-checking",
-  ],
-  ignorePatterns: ["*.js", "*.test.ts"],
-
-  overrides: [],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-    project: ["tsconfig.json"],
-  },
-  plugins: ["@typescript-eslint"],
-  rules: {},
-};
+    "env": {
+        "es2021": true,
+        "node": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "@typescript-eslint"
+    ],
+    "rules": {
+    }
+}
